479b588471e12893e1400a15fb5d415f65125fab
Antoine Bralet <antoine.bralet@tpg21.cpe.fr>
2020-10-07 08:41:40 +0200

    coords spatiales

--- Calibrage.py
+++ Calibrage.py
@@ -10,9 +10,18 @@ found, coord_px = cv2.findChessboardCorners(img, (7, 7), None)
 cv2.drawChessboardCorners(img, (7,7), coord_px, found)
 #de haut en bas de gauche à droite
 
+#Coordonnées des coins sur l'image en réalité (coordonnées objet de la mire)
+coord_mm = [[[20*i, 20*j] for j in range(7)] for i in range(7)]
+
+
+
+
+
 cv2.imshow('Capture_Affine', img) #affichage
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
 
 
 
